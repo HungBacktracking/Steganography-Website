1c6d8c6814aa1ee234ddba737cecc5e37340be0c
Võ Trung Hoàng Hưng <21120011@student.hcmus.edu.vn>
2024-06-13 19:12:02 +0700

    init image model

--- backend/controllers/image_controller.py
+++ backend/controllers/image_controller.py
@@ -1,7 +1,8 @@
 from flask import jsonify
+from models.image_model import Image
 
 
 def get_image_encoded(data):
     # This is where the image encoding will be done by calling function from the model
-    value = 1
+    value = Image.encode(data) # Dummy implementation
     return jsonify({"message": "Image encoded successfully", "data": value})
