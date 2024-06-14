fb2d3b30499a67f35a0b265a6a80a90bf6fe8165
Võ Trung Hoàng Hưng <21120011@student.hcmus.edu.vn>
2024-06-14 07:25:29 +0700

    feat: background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
 import Navbar from './layout/Navbar/Navbar';
 import HomePage from './pages/HomePage/HomePage';
@@ -8,20 +8,42 @@ import AboutPage from './pages/AboutPage/AboutPage';
 import ContactPage from './pages/ContactPage/ContactPage';
 import StegImagePage from './pages/StegImagePage/StegImagePage';
 
+import './App.css';
+
 const App = () => {
+	const location = useLocation();
+	
+	const getBackgroundClass = () => {
+		switch (location.pathname) {
+			case '/':
+				return 'home-page';
+			case '/steganography':
+				return 'background-container-steg steganography-page';
+			case '/steganography/image':
+				return 'steg-image-page';
+			case '/about':
+				return 'about-page';
+			case '/contact':
+				return 'contact-page';
+			default:
+				return '';
+		}
+	};
 
 	return (
-		<Router>
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/steganography" element={<SteganographyPage />} />
-				<Route path="/steganography/image" element={<StegImagePage />} />
-				<Route path="/about" element={<AboutPage />} />
-				<Route path="/contact" element={<ContactPage />} />
-			</Routes>
-		</Router>
+		<div className={`background-container ${getBackgroundClass()}`}>
+			<div className='overlay'>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/steganography" element={<SteganographyPage />} />
+					<Route path="/steganography/image" element={<StegImagePage />} />
+					<Route path="/about" element={<AboutPage />} />
+					<Route path="/contact" element={<ContactPage />} />
+				</Routes>
+			</div>
+		</div>
 	)
-}
+};
 
 export default App;
--- frontend/src/pages/StegImagePage/StegImagePage.jsx
+++ frontend/src/pages/StegImagePage/StegImagePage.jsx
@@ -4,8 +4,7 @@ import './StegImagePage.css';
 
 const StegImagePage = () => {
     return (
-        <div>
-            <h1>StegImagePage</h1>
+        <div className='container-page_'>
         </div>
     );
 }
