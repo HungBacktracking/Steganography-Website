e87baa34cdc82d0cdc3f3cada5b6bb1ea9283226
Võ Trung Hoàng Hưng <21120011@student.hcmus.edu.vn>
2024-06-15 08:20:42 +0700

    refactor components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,27 +11,9 @@ import StegImagePage from './pages/StegImagePage/StegImagePage';
 import './App.css';
 
 const App = () => {
-	const location = useLocation();
 	
-	const getBackgroundClass = () => {
-		switch (location.pathname) {
-			case '/':
-				return 'home-page';
-			case '/steganography':
-				return 'steganography-page';
-			case '/steganography/image':
-				return 'background-container-steg steg-image-page';
-			case '/about':
-				return 'about-page';
-			case '/contact':
-				return 'contact-page';
-			default:
-				return '';
-		}
-	};
-
 	return (
-		<div className={`background-container flex flex-col max-h-[100vh] ${getBackgroundClass()}`}>
+		<Router>
 			<Navbar />
 			<Routes>
 				<Route path="/" element={<HomePage />} />
@@ -40,7 +22,7 @@ const App = () => {
 				<Route path="/about" element={<AboutPage />} />
 				<Route path="/contact" element={<ContactPage />} />
 			</Routes>
-		</div>
+		</Router>
 	)
 };
 
