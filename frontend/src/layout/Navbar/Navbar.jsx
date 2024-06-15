31fb04f51c1ba9c51469752026c456b027f2c9a8
Võ Trung Hoàng Hưng <21120011@student.hcmus.edu.vn>
2024-06-15 07:33:34 +0700

    upgrade: change navbar

--- frontend/src/layout/Navbar/Navbar.jsx
+++ frontend/src/layout/Navbar/Navbar.jsx
@@ -6,25 +6,31 @@ const Navbar = () => {
     return (
         <div className={classes.navbar}>
             <div className={classes.navbar_wrapper}>
-                <div className={classes.navbar_item}>
-                    <NavLink className={({ isActive }) => isActive ? `${classes.nav_link} ${classes.active}` : classes.nav_link} to="/">
-                        Home
-                    </NavLink>
-                </div>
-                <div className={classes.navbar_item}>
-                    <NavLink className={({ isActive }) => isActive ? `${classes.nav_link} ${classes.active}` : classes.nav_link} to="/steganography">
-                        Let's Steganography
-                    </NavLink>
-                </div>
-                <div className={classes.navbar_item}>
-                    <NavLink className={({ isActive }) => isActive ? `${classes.nav_link} ${classes.active}` : classes.nav_link} to="/about">
-                        About
-                    </NavLink>
-                </div>
-                <div className={classes.navbar_item}>
-                    <NavLink className={({ isActive }) => isActive ? `${classes.nav_link} ${classes.active}` : classes.nav_link} to="/contact">
-                        Contact
-                    </NavLink>
+                <NavLink className={classes.navbar_logo} to="/">
+                    <div className={classes.logo}></div>
+                    <div className={classes.navbar_home}>Steganography</div>
+                </NavLink>
+                <div className={`${classes.navbar_list} ms-auto`}>
+                    <div className={classes.navbar_item}>
+                        <NavLink className={({ isActive }) => isActive ? `${classes.nav_link} ${classes.active}` : classes.nav_link} to="/">
+                            Home
+                        </NavLink>
+                    </div>
+                    <div className={classes.navbar_item}>
+                        <NavLink className={({ isActive }) => isActive ? `${classes.nav_link} ${classes.active}` : classes.nav_link} to="/steganography">
+                            Let's Steganography
+                        </NavLink>
+                    </div>
+                    <div className={classes.navbar_item}>
+                        <NavLink className={({ isActive }) => isActive ? `${classes.nav_link} ${classes.active}` : classes.nav_link} to="/about">
+                            About
+                        </NavLink>
+                    </div>
+                    <div className={classes.navbar_item}>
+                        <NavLink className={({ isActive }) => isActive ? `${classes.nav_link} ${classes.active}` : classes.nav_link} to="/contact">
+                            Contact
+                        </NavLink>
+                    </div>
                 </div>
             </div>
         </div>
