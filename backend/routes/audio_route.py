d0fe4ca02e2a25ea5ae57c98b171ffc65fbe27a8
Võ Trung Hoàng Hưng <21120011@student.hcmus.edu.vn>
2024-06-17 21:46:14 +0700

    feat: add audio decode route

--- backend/routes/audio_route.py
+++ backend/routes/audio_route.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from controllers.audio_controller import get_audio_encoded
+from controllers.audio_controller import get_audio_encoded, get_audio_decoded
 
 audio = Blueprint("audio", __name__)
 
@@ -7,6 +7,10 @@ audio = Blueprint("audio", __name__)
 @audio.route("/audio/encoded", methods=["POST"])
 def get_audio_encoded():
     return get_audio_encoded(request.json) # Example of how to use the audio function from the controller
+
+@audio.route("/audio/decoded", methods=["POST"])
+def get_audio_decoded():
+    return get_audio_decoded(request.json) # Example of how to use the audio function from the controller
 
 
 
