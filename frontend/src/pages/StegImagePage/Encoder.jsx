559594154827901715a014feaeaea713ce1bf534
Hoang C. Ta <taconghoang1103@gmail.com>
2024-06-20 01:24:18 +0700

    refactor: encode image  tab size : 2

--- frontend/src/pages/StegImagePage/Encoder.jsx
+++ frontend/src/pages/StegImagePage/Encoder.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 
 import classes from './StegImagePage.module.css';
@@ -11,11 +11,11 @@ const PASSWORD_POPUP = 'passwordPopup';
 
 class ImageData {
   constructor(image) {
-    this.name         = image.name;
-    this.base64encode = image.base64encode;
-    this.resolution   = image.resolution;
-    this.format       = image.format;
-    this.size         = image.size;
+    this.name         = image.name || "N/A";
+    this.base64encode = image.base64encode || "";
+    this.resolution   = image.resolution || "N/A";
+    this.format       = image.format || "N/A";
+    this.size         = image.size || "N/A";
   }
 }
 
@@ -52,41 +52,41 @@ const Encoder = ({ setActiveTab }) => {
   const [messageContent, setMessageContent] = useState("");
 
   return (
-      <div className={classes.steg_container}>
-          {/* Title */}
-          <div className={classes.list_title}>
-            <div className={ `${classes.title_active} ${classes.title}`}>Encode</div>
-            <div className={classes.title} onClick={() => setActiveTab('decode')}>Decode</div>
-          </div>
-          
-          <div className={classes.steg_wrapper}>
-              <EncoderLeftComponent 
-                togglePopup={togglePopup}
-              />
-
-              <EncoderRightComponent 
-                messageContent={messageContent}
-                setMessageContent={setMessageContent}   
-              />
-          </div>
-
-          <div>
-            {
-              isOpen(PASSWORD_POPUP) && (
-                <PasswordPopup
-                  onConfirm={(password) => {
-                    handleEncode("data", password);
-                  }
-                }
-                  onCancel={() => {
-                    toast.warning("Encode canceled");
-                  }}
-                  onClose={() => togglePopup(PASSWORD_POPUP)}
-                />
-              )
-            }
-          </div>
+    <div className={classes.steg_container}>
+      {/* Title */}
+      <div className={classes.list_title}>
+        <div className={ `${classes.title_active} ${classes.title}`}>Encode</div>
+        <div className={classes.title} onClick={() => setActiveTab('decode')}>Decode</div>
       </div>
+      
+      <div className={classes.steg_wrapper}>
+        <EncoderLeftComponent 
+          togglePopup={togglePopup}
+        />
+
+        <EncoderRightComponent 
+          messageContent={messageContent}
+          setMessageContent={setMessageContent}   
+        />
+      </div>
+
+      <div>
+        {
+          isOpen(PASSWORD_POPUP) && (
+            <PasswordPopup
+              onConfirm={(password) => {
+                handleEncode("data", password);
+              }
+            }
+              onCancel={() => {
+                toast.warning("Encode canceled");
+              }}
+              onClose={() => togglePopup(PASSWORD_POPUP)}
+            />
+          )
+        }
+      </div>
+    </div>
   )
 }
 
@@ -95,10 +95,7 @@ const EncoderLeftComponent = ({
 }) => {
 
   const fileInput = useRef(null);
-  const [image, setImage] = useState(null);
-  const [imageResolution, setImageResolution] = useState("N/A");
-  const [imageFormat, setImageFormat] = useState("N/A");
-  const [imageSize, setImageSize] = useState("N/A");
+  const [imageData, setImageData] = useState(new ImageData({}));
 
   const handleImageContainerClick = () => {
     fileInput.current.click();
@@ -108,91 +105,84 @@ const EncoderLeftComponent = ({
     const file = e.target.files[0];
     
     if (file) {
-      // Set the image to be displayed
-      setImageFormat(file.type.split('/')[1].toUpperCase());
-      setImageSize((file.size / 1024 / 1024).toFixed(2) + 'Mb'); // Convert bytes to megabytes
-
       const reader = new FileReader();
       reader.onload = (e) => {
-        setImage(e.target.result);
-        // alert("File uploaded successfully!");
-
-        // Get image resolution
         const img = new Image();
         img.onload = () => {
-          setImageResolution(`${img.width}x${img.height}`);
+
+          // read data from image uploaded
+          const imageData = new ImageData({
+            name: file.name,
+            base64encode: e.target.result,
+            resolution: `${img.width} x ${img.height}`,
+            format: file.type.split('/')[1].toUpperCase(),
+            size: `${(file.size / 1024 / 1024).toFixed(2)} MB`,
+          });
+          setImageData(imageData);
+          
         };
         img.src = e.target.result;
       };
 
       reader.readAsDataURL(file);
-      console.log(file);
       // do something with the file
 
     }
     else {
       alert("Failed to upload file. Please try again.");
-      console.log("No file selected");
     }
   };
 
   return (
-      <div className={classes.left}>
-          <div className={classes.image_container} onClick={handleImageContainerClick}>
-              <div className={classes.uploadPrompt}>
-                { image 
-                  ? (<img src={image} alt="Uploaded" />)
-                  : (<>
-                    <img src={UploadImage} className='' alt="Upload" />
-                    <p className="">Click to upload an image</p>
-                  </>) }
-              </div>
-            <input
-              type="file"
-              ref={fileInput}
-              onChange={handleFileSelection}
-              style={{ display: 'none' }}
-              accept="image/*" // Accept only images
-            />
+    <div className={classes.left}>
+      <div className={classes.image_container} onClick={handleImageContainerClick}>
+          <div className={classes.uploadPrompt}>
+            { imageData.base64encode 
+              ? (<img src={imageData.base64encode} alt="Uploaded" />)
+              : (<>
+                <img src={UploadImage} className='' alt="Upload" />
+                <p className="">Click to upload an image</p>
+              </>) }
           </div>
-          
-          {/* <div className={classes.info}>Resolution</div> */}
-          <TwoSideTextBox 
-            titleComponent={<div className="basis-1/3" >Resolution</div>}
-            content={imageResolution}
-          />
-
-          {/* <div className={classes.info}>Mode</div> */}
-          {/* <TwoSideTextBox
-            titleComponent={<div className="basis-1/3" >Mode</div>}
-            content={"...."}
-          /> */}
-
-
-          {/* <div className={classes.info}>Format</div> */}
-          <TwoSideTextBox
-            titleComponent={<div className="basis-1/3" >Format</div>}
-            content={imageFormat}
-          />
-
-           {/* <div className={classes.info}>Size</div> */}
-           <TwoSideTextBox
-            titleComponent={<div className="basis-1/3" >Size</div>}
-            content={imageSize}
-          />
-
-          <div className={classes.action}>
-
-              <div 
-                className={`${classes.button_action_1} ${classes.success_}`}
-                onClick={() => togglePopup(PASSWORD_POPUP)}
-              >
-                Encode
-              </div>
-              
-              <div className={`${classes.button_action_1} ${classes.destroy_}`}>Delete</div>
-          </div>
+        <input
+          type="file"
+          ref={fileInput}
+          onChange={handleFileSelection}
+          style={{ display: 'none' }}
+          accept="image/*" // Accept only images
+        />
       </div>
+      
+      {/* Resolution */}
+      <TwoSideTextBox 
+        titleComponent={<div className="basis-1/3" >Resolution</div>}
+        content={imageData.resolution}
+      />
+
+      {/* Format */}
+      <TwoSideTextBox
+        titleComponent={<div className="basis-1/3" >Format</div>}
+        content={imageData.format}
+      />
+
+        {/* Size */}
+        <TwoSideTextBox
+        titleComponent={<div className="basis-1/3" >Size</div>}
+        content={imageData.size}
+      />
+
+      <div className={classes.action}>
+
+        <div 
+          className={`${classes.button_action_1} ${classes.success_}`}
+          onClick={() => togglePopup(PASSWORD_POPUP)}
+        >
+          Encode
+        </div>
+        
+        <div className={`${classes.button_action_1} ${classes.destroy_}`}>Delete</div>
+      </div>
+    </div>
   );
 }
 
@@ -295,39 +285,39 @@ const EncoderRightComponent = ({
   }
 
   return (
-      <div className={classes.right}>
-          <div className={classes.header_notepad}>
-              <div className={classes.small_title}>Notepad</div>
-              <div className={`${classes.action_list} ms-auto`}>
-                  <div className={classes.button_action_2}
-                    onClick={(e) => {
-                      e.target.nextElementSibling.click();
-                    } }>
-                    Open
-                  </div>
-                  <input type="file" style={{ display: 'none' }} 
-                    accept=".txt"
-                    onChange={handleFileChange}
-                    multiple={false}
-                  />
-                  <div className={classes.button_action_2}>Save</div>
-                  <div className={classes.button_action_2}>Save as</div>
-                  <div className={classes.button_action_2}>New</div>
-              </div>
+    <div className={classes.right}>
+      <div className={classes.header_notepad}>
+        <div className={classes.small_title}>Notepad</div>
+        <div className={`${classes.action_list} ms-auto`}>
+          <div className={classes.button_action_2}
+            onClick={(e) => {
+              e.target.nextElementSibling.click();
+            } }>
+            Open
           </div>
-          <textarea 
-            className={classes.notepad} 
-            onChange={(e) => { setMessageContent(e.target.value) }}
-            value={messageContent}
-          >
-          </textarea>
-          <div className={classes.info_list + " mt-auto"}>
-              {/* <div className={classes.info_item_capacity}>Capacity</div> */}
-              <TwoSideTextBox className={"flex-[70%] p-[0.7vw]"} title="Capacity" content="2.1Kb/ 10Kb" />
-              {/* <div className={classes.info_item_path}>Path</div> */}
-              <TwoSideTextBox className={"flex-[70%] p-[0.7vw]"} title="Path" content="D:\path\sub_path" />
-          </div>
+          <input type="file" style={{ display: 'none' }} 
+            accept=".txt"
+            onChange={handleFileChange}
+            multiple={false}
+          />
+          <div className={classes.button_action_2}>Save</div>
+          <div className={classes.button_action_2}>Save as</div>
+          <div className={classes.button_action_2}>New</div>
+        </div>
       </div>
+      <textarea 
+        className={classes.notepad} 
+        onChange={(e) => { setMessageContent(e.target.value) }}
+        value={messageContent}
+      >
+      </textarea>
+      <div className={classes.info_list + " mt-auto"}>
+          {/* <div className={classes.info_item_capacity}>Capacity</div> */}
+          <TwoSideTextBox className={"flex-[70%] p-[0.7vw]"} title="Capacity" content="2.1Kb/ 10Kb" />
+          {/* <div className={classes.info_item_path}>Path</div> */}
+          <TwoSideTextBox className={"flex-[70%] p-[0.7vw]"} title="Path" content="D:\path\sub_path" />
+      </div>
+    </div>
   )
 }
 
