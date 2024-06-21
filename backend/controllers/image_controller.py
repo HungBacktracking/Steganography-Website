73bb313200f3f9c336d64641c33fef53c1fedd15
NTTVy03 <trucvy05062003@gmail.com>
2024-06-21 19:34:00 +0700

    test: done encoded/decoded image

--- backend/controllers/image_controller.py
+++ backend/controllers/image_controller.py
@@ -1,26 +1,59 @@
-from flask import jsonify, send_file, request
+from flask import jsonify, send_file
 from models.image_model import ImageSteganography
+import base64
+import io
 
-
-def get_image_encoded():
+def get_encoded_image(request):
+    print("---------------- Try encode image")
+    # print("request in controller")
+    # print(request)
     try:
-        image_file = request.files['image']
-        message = request.form['message']
+        print("Get data from request")
+        image_base64 = request['image']
+        message = request['message']
 
+        # Decode the base64-encoded image
+        print("Decode the base64-encoded image")
+        image_data = base64.b64decode(image_base64)
+        image_file = io.BytesIO(image_data)
+
+        print("Call encode")
         stego = ImageSteganography()
         modified_image = stego.hide_data(image_file, message)
 
-        return send_file(modified_image, attachment_filename='stego_image.png', as_attachment=True, mimetype='image/png')
-    except Exception as e:
-        return jsonify({'error': str(e)})
-    
-def get_image_decoded():
-    try:
-        image_file = request.files['image']
+        # Save the image to a BytesIO object
+        print("Save the image to a BytesIO object")
+        image_io = io.BytesIO()
+        modified_image.save(image_io, format='PNG')
+        image_io.seek(0)
 
+        # Encode the modified image back to base64
+        print("Encode result image")
+        encoded_image = base64.b64encode(image_io.getvalue()).decode()
+
+        print("Return response")
+        return jsonify({'image': encoded_image})
+    except Exception as e:
+        print("ERROR 400")
+        return jsonify({'error': str(e)}), 400
+
+def get_decoded_image(request):
+    print("---------------- Try decode image")
+    try:
+        print("Get data from request json")
+        image_base64 = request['image']
+
+        # Decode the base64-encoded image
+        print("Decode the base64-encoded image")
+        image_data = base64.b64decode(image_base64)
+        image_file = io.BytesIO(image_data)
+
+        print("Call decode")
         stego = ImageSteganography()
         message = stego.recover_data(image_file)
 
+        print("Return response")
         return jsonify({'message': message})
     except Exception as e:
-        return jsonify({'error': str(e)})
+        print("ERROR 400")
+        return jsonify({'error': str(e)}), 400
--- backend/models/image_model.py
+++ backend/models/image_model.py
@@ -1,52 +1,38 @@
-
 import re
-from tkinter import Image
-
+from PIL import Image  # Use PIL instead of tkinter.Image
 
 class ImageSteganography:
     def __init__(self):
         self.num_lsb = 8
 
-    # Convert encoding data into 8-bit binary ASCII
-    def generateData(data):
+    def generateData(self, data):
         newdata = []
-        for i in data: # list of binary codes of given data
+        for i in data:
             newdata.append(format(ord(i), '08b'))
         return newdata
-    
-    # Pixels modified according to encoding data in generateData
-    # this function modify 1 frame
+
     def modifyPixel(self, pixel, data):
-        # Embedded data: list of binary (1d array [n]) --> list of 8-bit binary (2d array [n][8])
         datalist = self.generateData(data)
         lengthofdata = len(datalist)
         imagedata = iter(pixel)
         for i in range(lengthofdata):
-            # pixel = Get 9 values (3 colors RGB * 3 pixels) from the cover image
-            # For the 8-first values of pixel, embed data: cover = odd --> embeded = 1; cover = even --> embeded = 0
-            # For the 9th value: mark if the end of embedded data: 0 --> continue; 1 --> stop
-
-            # Extracts 3 pixels at a time; len(pixel) = 9 (3 colors RGB * 3 pixels)
-            pixel = [value for value in imagedata.__next__()[:3] + imagedata.__next__()[:3] + imagedata.__next__()[:3]]
-            # print(pixel)
-            # Pixel value should be made odd for 1 and even for 0
+            pixel = [value for value in next(imagedata)[:3] + next(imagedata)[:3] + next(imagedata)[:3]]
             for j in range(0, 8):
-                if (datalist[i][j] == '0' and pixel[j]% 2 != 0):
+                if (datalist[i][j] == '0' and pixel[j] % 2 != 0):
                     pixel[j] -= 1
                 elif (datalist[i][j] == '1' and pixel[j] % 2 == 0):
-                    if(pixel[j] != 0):
+                    if pixel[j] != 0:
                         pixel[j] -= 1
                     else:
                         pixel[j] += 1
-            # Eighth pixel of every set tells whether to stop ot read further. 0 means keep reading; 1 means thec message is over.
-            if (i == lengthofdata - 1):
-                if (pixel[-1] % 2 == 0):
-                    if(pixel[-1] != 0):
+            if i == lengthofdata - 1:
+                if pixel[-1] % 2 == 0:
+                    if pixel[-1] != 0:
                         pixel[-1] -= 1
                     else:
                         pixel[-1] += 1
             else:
-                if (pixel[-1] % 2 != 0):
+                if pixel[-1] % 2 != 0:
                     pixel[-1] -= 1
             pixel = tuple(pixel)
             yield pixel[0:3]
@@ -58,47 +44,32 @@ class ImageSteganography:
         (x, y) = (0, 0)
     
         for pixel in self.modifyPixel(newimage.getdata(), data):
-    
-            # Putting modified pixels in the new image
             newimage.putpixel((x, y), pixel)
-            if (x == w - 1):
+            if x == w - 1:
                 x = 0
                 y += 1
             else:
                 x += 1
 
-    def hide_data(self, image_path, message):
-        try:
-            image = Image.open(image_path, 'r') # Parameter has to be r, otherwise ValueError will occur (https://pillow.readthedocs.io/en/stable/reference/Image.html)
-        except FileNotFoundError:
-            raise ValueError("\n%s not found! Exiting..." % image_path)
-        
-        input_data = memoryview(message.encode('utf-8'))
-
-        newimage = image.copy() # New Variable to Store Hiddend Data
-        self.encoder(newimage, input_data) # Steganography
-        new_img_name = "encoded_image.png"
-        newimage.save(new_img_name) # Save as New Frame
-
+    def hide_data(self, image_file, message):
+        image = Image.open(image_file)
+        newimage = image.copy()
+        self.encoder(newimage, message)
         return newimage
     
-    def recover_data(self, image_path):
-        try:
-            data = ''
-            image = Image.open(image_path, 'r')
-            imagedata = iter(image.getdata())
-            while (True):
-                pixels = [value for value in imagedata.__next__()[:3] + imagedata.__next__()[:3] + imagedata.__next__()[:3]]
-                # string of binary data
-                binstr = ''
-                for i in pixels[:8]:
-                    if (i % 2 == 0):
-                        binstr += '0'
-                    else:
-                        binstr += '1'
-                if re.match("[ -~]", chr(int(binstr,2))) is not None: # only decode printable data
-                    data += chr(int(binstr, 2))
-                if (pixels[-1] % 2 != 0):
-                    return data
-        except StopIteration:
-            raise ValueError("No data found in %s" % image_path)
+    def recover_data(self, image_file):
+        data = ''
+        image = Image.open(image_file)
+        imagedata = iter(image.getdata())
+        while True:
+            pixels = [value for value in next(imagedata)[:3] + next(imagedata)[:3] + next(imagedata)[:3]]
+            binstr = ''
+            for i in pixels[:8]:
+                if i % 2 == 0:
+                    binstr += '0'
+                else:
+                    binstr += '1'
+            if re.match("[ -~]", chr(int(binstr, 2))) is not None:
+                data += chr(int(binstr, 2))
+            if pixels[-1] % 2 != 0:
+                return data
