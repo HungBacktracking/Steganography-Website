69ed4c4dd48c850539b5b856457642eabd6b8f01
quangphat18ti <quangphat18ti@gmail.com>
2024-07-24 16:56:46 +0700

    feat: Add props for UploadComponent

--- frontend/src/components/UploadComponent/UploadComponent.jsx
+++ frontend/src/components/UploadComponent/UploadComponent.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { UploadContainer } from '../../assets';
 import classes from './UploadComponent.module.css';
 
-function UploadComponent({ readDataUploaded, fileInput }) {
+function UploadComponent({ 
+  readDataUploaded, 
+  fileInput,
+  accept }) {
   return (
     <div className={classes.upload_container} onClick={() => fileInput.current && fileInput.current.click()}>
       <div className={classes.uploadPrompt}>
@@ -15,7 +18,7 @@ function UploadComponent({ readDataUploaded, fileInput }) {
         ref={fileInput}
         onChange={readDataUploaded}
         style={{ display: 'none' }}
-        accept="image/*" // Accept only images
+        accept={accept  }
       />
     </div>
   );
